ab739a48c4f66b7c511c6dbf066d9e37e030f7f7
Shamsunnahar Afifa <shamsunnaharafifa@gmail.com>
2019-10-05 23:30:10 +0600

    Quote

--- MVVMBasic/app/src/main/java/com/example/afifa123/mvvmbasic/data/FakeDatabase.kt
+++ MVVMBasic/app/src/main/java/com/example/afifa123/mvvmbasic/data/FakeDatabase.kt
@@ -1,4 +1,21 @@
 package com.example.afifa123.mvvmbasic.data
 
-class FakeDatabase {
+class FakeDatabase  private constructor(){
+    //All the DOA's go here!
+    var quoteDao = FakeQuoteDao()
+        private set
+    companion object{
+
+        //@volatile writes this property are immediately visible to other threads
+        @Volatile private var instance : FakeDatabase? = null
+
+        //The only way to get hold the FakeDatabase
+        fun getInstance() =
+        //Already instantiated? - return the instance
+            //Otherwise instantiated in a thread manner
+            instance ?: synchronized(this) {
+
+                instance ?: FakeDatabase().also { instance = it }
+            }
+
 }
